7ba0f5d8c46fd1850dd1d1a81e61e094c794cae6
cs1867 <blackhatracing@gmail.com>
2025-12-10 16:29:37 +0000

    update troubleshoot deb repo build

--- toolbox/workflows/create-db-repo.sh
+++ toolbox/workflows/create-db-repo.sh
@@ -4,21 +4,31 @@ set -e
 echo "** Run create deb repo sh **"
 
 file="/build/variables.txt"
-# Load variables (repo, os_dir, deb_codename, etc.)
+echo "Before sourcing:"
+env | sort
+
+echo "Sourcing variables from $file"
 . "$file"
+
+echo "After sourcing:"
+env | sort
 
 echo "Create a Debian/Ubuntu repo to be uploaded"
 
 cd artifacts
+pwd
+ls
 
 echo "apt-get install dpkg-dev"
 sudo apt-get update
 sudo apt-get install -y dpkg-dev
 
-#    Example: /repo/nightly/deb/$os_dir/$deb_codename
+final_cache_path="$repo/$os_dir/"
 REPO_ROOT="$repo/$os_dir/$deb_codename"
 echo "Repo root: $REPO_ROOT"
+echo "final cache path root: $final_cache_path"
 
+exit
 mkdir -p "$REPO_ROOT"
 
 cp DEBS/*.deb "$REPO_ROOT"/
