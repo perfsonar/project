a619edb1f34da0cb9bf3455df9f1f979671d9918
cs1867 <blackhatracing@gmail.com>
2025-12-10 19:51:43 +0000

    changes for deb repo builds

--- toolbox/workflows/create-db-repo.sh
+++ toolbox/workflows/create-db-repo.sh
@@ -1,45 +1,31 @@
 #!/usr/bin/env bash
-set -e
+#set -e
 
-echo "** Run create deb repo sh **"
+echo "** Create deb repo sh **"
 
 file="/build/variables.txt"
 echo "cat variables.txt"
 cat /build/variables.txt
-echo "Before sourcing:"
-env | sort
 
-echo "Sourcing variables from $file"
 source $file
 
-echo "After sourcing:"
-env | sort
-
-echo "Create a Debian/Ubuntu repo to be uploaded"
 
 cd artifacts
-pwd
-ls
 
 echo "apt-get install dpkg-dev"
 sudo apt-get update
 sudo apt-get install -y dpkg-dev
 
 final_cache_path="$repo/$os_dir/"
-REPO_ROOT="$repo/$os_dir/$deb_codename"
-echo "Repo root: $REPO_ROOT"
-echo "final cache path root: $final_cache_path"
+echo "Repo path: $final_cache_path"
+
+ls $final_cache_path
 
 exit
-mkdir -p "$REPO_ROOT"
 
-cp DEBS/*.deb "$REPO_ROOT"/
-
-cd "$REPO_ROOT"
 
 echo "Generating Packages index..."
 dpkg-scanpackages . /dev/null > Packages
-gzip -kf Packages
 
 echo "Contents of repo directory:"
 ls -l
