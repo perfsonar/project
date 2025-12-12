baacaecc39542d1cc251f05a0e9c33d0d926aeef
cs1867 <blackhatracing@gmail.com>
2025-12-12 14:37:54 +0000

    working on deb repo II

--- toolbox/workflows/create-db-repo.sh
+++ toolbox/workflows/create-db-repo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #set -e
 
-echo "** Create deb repo sh **"
+echo "** build deb repo sh **"
 
 file="/build/variables.txt"
 echo "cat variables.txt"
@@ -9,27 +9,24 @@ cat /build/variables.txt
 
 source $file
 
-
-cd artifacts
+cd artifacts/debian
 
 echo "apt-get install dpkg-dev"
 sudo apt-get update
 sudo apt-get install -y dpkg-dev
 
-final_cache_path="$repo/$os_dir/"
-echo "Repo path: $final_cache_path"
-ls $final_cache_path
-
-echo "ls build dir"
-ls -al build
-exit
-
-
 echo "Generating Packages index..."
 dpkg-scanpackages . /dev/null > Packages
+
+echo "Generating compressed Packages.gz..."
+gzip -9c Packages > Packages.gz
+
+echo "Generating Release file..."
+apt-ftparchive release . > Release
+
 
 echo "Contents of repo directory:"
 ls -l
 
-echo "** End create deb repo sh **"
+echo "** End build deb repo sh **"
 
