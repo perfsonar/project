353837bfedb22d31ec7a6d66e66ac02a7400a9fe
cs1867 <blackhatracing@gmail.com>
2026-04-21 17:55:13 +0000

    updates for deb/ubn repos

--- toolbox/workflows/create-db-repo.sh
+++ toolbox/workflows/create-db-repo.sh
@@ -16,7 +16,8 @@ cat "$file"
 SRC_DIR="artifacts/debian"
 REPO_DIR="/var/local/repo"
 
-DIST="${DIST:-perfsonar-5.3-staging}"
+#TODO: detemine this dynamic nightly build vs tag
+DIST="${DIST:-perfsonar-5.3-snapshot}"
 COMPONENT="${COMPONENT:-main}"
 ARCH="${ARCH:-amd64}"
 ORIGIN="${ORIGIN:-perfSONAR}"
@@ -42,28 +43,26 @@ sudo cp "/build/project/toolbox/workflows/distributions" "$REPO_DIR/conf/distrib
 
 echo "Copying deb files into repo working area..."
 sudo mkdir -p "$REPO_DIR"
-sudo find "$REPO_DIR" -maxdepth 1 -type f -name '*.deb' -exec rm -f {} \;
-sudo cp -v "$SRC_DIR"/*.deb "$REPO_DIR"/
 
 echo "Contents of $REPO_DIR before import:"
 sudo ls -l "$REPO_DIR"
 
 echo "Adding packages to reprepro..."
-found_deb=0
-for deb in "$REPO_DIR"/*.deb; do
-    if [ ! -f "$deb" ]; then
-        continue
-    fi
-
-    found_deb=1
-    echo "Including $deb"
-    sudo reprepro -b "$REPO_DIR" includedeb "$DIST" "$deb"
-done
-
-if [ "$found_deb" -eq 0 ]; then
-    echo "ERROR: No .deb files found in $REPO_DIR"
-    exit 1
-fi
+#found_deb=0
+#for deb in "$REPO_DIR"/*.deb; do
+#    if [ ! -f "$deb" ]; then
+#        continue
+#    fi
+#
+#    found_deb=1
+#    echo "Including $deb"
+#    sudo reprepro -b "$REPO_DIR" includedeb "$DIST" "$deb"
+#done
+#
+#if [ "$found_deb" -eq 0 ]; then
+#    echo "ERROR: No .deb files found in $REPO_DIR"
+#    exit 1
+#fi
 
 echo "Final repo contents:"
 echo $REPO_DIR
@@ -71,9 +70,8 @@ echo $REPO_DIR
 sudo find "$REPO_DIR" -maxdepth 3 -type f | sort
 pwd
 ls -al artifacts
-sudo mkdir ./artifacts/repo
-sudo mv "$REPO_DIR"/* ./artifacts/repo/
-echo "list var local/repo"
-echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
-ls -al ./artifacts/repo
+
+#sudo mkdir ./artifacts/repo
+#sudo mv "$REPO_DIR"/* ./artifacts/repo/
+
 echo "** End build deb repo with reprepro **"
